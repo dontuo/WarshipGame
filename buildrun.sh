c02723e5e9b39abde5cabfe9e5e4302647b3cc9b
dontuo <bench.oleg@gmail.com>
2024-08-19 01:47:56 +0300

    add sh script (for my nvim config)

---
+++ buildrun.sh
@@ -0,0 +1,12 @@
+rm MinecraftClone
+cmake CMakeLists.txt -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+make -j8
+
+if [ ! -f MinecraftClone ]; then
+    echo "Compile failed"
+else
+    echo "Starting game: "
+    ./WarshipGame
+fi
+
