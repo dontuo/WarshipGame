7184a8770e1ad0c2565c8944bf0dc66bd684b9bc
dontuo <bench.oleg@gmail.com>
2024-08-23 23:22:29 +0300

    add grid class

---
+++ src/grid.cpp
@@ -0,0 +1,39 @@
+#include "grid.h"
+
+Grid::Grid(int row, int column, int size, int offsetX, int offsetY)
+    :mRow(row),mColumn(column),mSize(size),mOffsetX(offsetX),mOffsetY(offsetY)
+{}
+
+void DrawGrid(int row, int column, int size, int offsetX, int offsetY)
+{
+    for(int x = 0; x <= row; x++)
+        DrawLine(x * size + offsetX,offsetY,x * size + offsetX, column * size + offsetY, WHITE);
+    
+    for(int y = 0; y <= column; y++)
+        DrawLine(offsetX - 1,y * size + offsetY,row * size + offsetX,y * size + offsetY,WHITE);
+}
+
+void Grid::Draw()
+{
+    for(int x = 0; x <= mRow; x++)
+        DrawLine(x * mSize + mOffsetX, mOffsetY,x * mSize + mOffsetX, mColumn * mSize + mOffsetY, WHITE);
+    
+    for(int y = 0; y <= mColumn; y++)
+        DrawLine(mOffsetX - 1,y * mSize + mOffsetY, mRow * mSize + mOffsetX,y * mSize + mOffsetY,WHITE);
+}
+
+
+void Grid::SetOffset(int offsetX, int offsetY)
+{
+    mOffsetX = offsetX;
+    mOffsetY = offsetY;
+}
+void Grid::SetRowColumn(int row, int column)
+{
+    mRow = row;
+    mColumn = column;
+}
+void Grid::SetSize(int size)
+{
+    mSize = size;
+}
