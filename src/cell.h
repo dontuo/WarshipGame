95543714e5f87b8bebf6b32e4e182a7b292cbd29
dontuo <bench.oleg@gmail.com>
2024-08-28 16:56:15 +0300

    add enum cellstate

---
+++ src/cell.h
@@ -0,0 +1,17 @@
+#pragma once
+
+enum CellState
+{
+    EMPTY,
+    BOAT_1,
+    BOAT_2,
+    BOAT_3,
+    BOAT_4,
+    BOAT_1_HITTED,
+    BOAT_2_HITTED,
+    BOAT_3_HITTED,
+    BOAT_4_HITTED,
+    HIT,
+    MISSED,
+    ELEMENT_COUNT
+};
