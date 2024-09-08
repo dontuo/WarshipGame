5bb5dc8bbab0542fcb12a9324a58cfc53aee4ccb
dontuo <bench.oleg@gmail.com>
2024-09-08 20:49:34 +0300

    changed name from ship to boat

--- src/ship.cpp
+++ src/ship.cpp
@@ -1,16 +1,20 @@
 #include "ship.h"
+#include "cell.h"
+#include "raylib.h"
+
+Texture2D textures[CellState::ELEMENT_COUNT];
 
 void LoadTextures()
 {
     textures[CellState::EMPTY] = LoadTexture("textures/empty.png"); 
-    textures[CellState::BOAT_1] = LoadTexture("textures/ship_1.png"); 
-    textures[CellState::BOAT_2] = LoadTexture("textures/ship_2.png"); 
-    textures[CellState::BOAT_3] = LoadTexture("textures/ship_3.png"); 
-    textures[CellState::BOAT_4] = LoadTexture("textures/ship_4.png"); 
-    textures[CellState::BOAT_1_HITTED] = LoadTexture("textures/ship_1_hitted.png"); 
-    textures[CellState::BOAT_2_HITTED] = LoadTexture("textures/ship_2_hitted.png"); 
-    textures[CellState::BOAT_3_HITTED] = LoadTexture("textures/ship_3_hitted.png"); 
-    textures[CellState::BOAT_4_HITTED] = LoadTexture("textures/ship_4_hitted.png"); 
+    textures[CellState::SHIP_1] = LoadTexture("textures/ship_1.png"); 
+    textures[CellState::SHIP_2] = LoadTexture("textures/ship_2.png"); 
+    textures[CellState::SHIP_3] = LoadTexture("textures/ship_3.png"); 
+    textures[CellState::SHIP_4] = LoadTexture("textures/ship_4.png"); 
+    textures[CellState::SHIP_1_HITTED] = LoadTexture("textures/ship_1_hitted.png"); 
+    textures[CellState::SHIP_2_HITTED] = LoadTexture("textures/ship_2_hitted.png"); 
+    textures[CellState::SHIP_3_HITTED] = LoadTexture("textures/ship_3_hitted.png"); 
+    textures[CellState::SHIP_4_HITTED] = LoadTexture("textures/ship_4_hitted.png"); 
     textures[CellState::HIT] = LoadTexture("textures/hit.png"); 
     textures[CellState::MISSED] = LoadTexture("textures/miss.png");
 }
@@ -19,26 +23,26 @@ void DrawShip(CellState type, int offsetX, int offsetY, Color color)
 {
     switch (type)
     {
-        case CellState::BOAT_1:
-            DrawTexture(textures[CellState::BOAT_1], offsetX, offsetY, color);
+        case CellState::SHIP_1:
+            DrawTexture(textures[CellState::SHIP_1], offsetX, offsetY, color);
         break;
         
-        case CellState::BOAT_2:
-            DrawTexture(textures[CellState::BOAT_2], offsetX,offsetY, color);
-            DrawTexture(textures[CellState::BOAT_2], offsetX + 44, offsetY, color);
+        case CellState::SHIP_2:
+            DrawTexture(textures[CellState::SHIP_2], offsetX,offsetY, color);
+            DrawTexture(textures[CellState::SHIP_2], offsetX + 44, offsetY, color);
         break;
 
-        case CellState::BOAT_3:
-            DrawTexture(textures[CellState::BOAT_3], offsetX, offsetY, color);
-            DrawTexture(textures[CellState::BOAT_3], offsetX + 44, offsetY, color);
-            DrawTexture(textures[CellState::BOAT_3], offsetX + 44 * 2, offsetY, color);
+        case CellState::SHIP_3:
+            DrawTexture(textures[CellState::SHIP_3], offsetX, offsetY, color);
+            DrawTexture(textures[CellState::SHIP_3], offsetX + 44, offsetY, color);
+            DrawTexture(textures[CellState::SHIP_3], offsetX + 44 * 2, offsetY, color);
         break;
 
-        case CellState::BOAT_4:
-            DrawTexture(textures[CellState::BOAT_4], offsetX, offsetY, color);
-            DrawTexture(textures[CellState::BOAT_4], offsetX + 44, offsetY, color);
-            DrawTexture(textures[CellState::BOAT_4], offsetX + 44 * 2, offsetY, color);
-            DrawTexture(textures[CellState::BOAT_4], offsetX + 44 * 3, offsetY, color);
+        case CellState::SHIP_4:
+            DrawTexture(textures[CellState::SHIP_4], offsetX, offsetY, color);
+            DrawTexture(textures[CellState::SHIP_4], offsetX + 44, offsetY, color);
+            DrawTexture(textures[CellState::SHIP_4], offsetX + 44 * 2, offsetY, color);
+            DrawTexture(textures[CellState::SHIP_4], offsetX + 44 * 3, offsetY, color);
         break;
     } 
 }
