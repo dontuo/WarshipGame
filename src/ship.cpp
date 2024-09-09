ce1bf8ad06b2d4abeb4fc41a412c8af81f78416a
dontuo <bench.oleg@gmail.com>
2024-09-09 22:27:25 +0300

    added function

--- src/ship.cpp
+++ src/ship.cpp
@@ -1,8 +1,9 @@
 #include "ship.h"
 #include "cell.h"
 #include "raylib.h"
+#include "raymath.h"
 
-Texture2D textures[CellState::ELEMENT_COUNT];
+Texture2D textures[CellState::ELEMENT_COUNT] = {};
 
 void LoadTextures()
 {
@@ -43,6 +44,35 @@ void DrawShip(CellState type, int offsetX, int offsetY, Color color)
             DrawTexture(textures[CellState::SHIP_4], offsetX + 44, offsetY, color);
             DrawTexture(textures[CellState::SHIP_4], offsetX + 44 * 2, offsetY, color);
             DrawTexture(textures[CellState::SHIP_4], offsetX + 44 * 3, offsetY, color);
+        break;
+    } 
+}
+
+
+void DrawShip(CellState type, Vector2 offset, Color color)
+{
+    switch (type)
+    {
+        case CellState::SHIP_1:
+            DrawTexture(textures[CellState::SHIP_1], offset.x, offset.y, color);
+        break;
+        
+        case CellState::SHIP_2:
+            DrawTexture(textures[CellState::SHIP_2], offset.x,offset.y, color);
+            DrawTexture(textures[CellState::SHIP_2], offset.x + 44, offset.y, color);
+        break;
+
+        case CellState::SHIP_3:
+            DrawTexture(textures[CellState::SHIP_3], offset.x, offset.y, color);
+            DrawTexture(textures[CellState::SHIP_3], offset.x + 44, offset.y, color);
+            DrawTexture(textures[CellState::SHIP_3], offset.x + 44 * 2, offset.y, color);
+        break;
+
+        case CellState::SHIP_4:
+            DrawTexture(textures[CellState::SHIP_4], offset.x, offset.y, color);
+            DrawTexture(textures[CellState::SHIP_4], offset.x + 44, offset.y, color);
+            DrawTexture(textures[CellState::SHIP_4], offset.x + 44 * 2, offset.y, color);
+            DrawTexture(textures[CellState::SHIP_4], offset.x + 44 * 3, offset.y, color);
         break;
     } 
 }
--- src/ship.h
+++ src/ship.h
@@ -5,7 +5,9 @@
 #include <iostream>
 
 extern Texture2D textures[];
+
 void DrawShip(CellState type, int offsetX, int offsetY, Color color);
+void DrawShip(CellState type, Vector2 offset, Color color);
 
 void LoadTextures();
 
@@ -14,4 +16,6 @@ struct Ship
 {
     Vector2 mPos;
     CellState mCellState;
+    bool rotate = 0;
+    //if rotate == 1 then rotate ship by 90 degree
 };
